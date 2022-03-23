92de66d0e82967b0fadb0e2ed2163094665d741f
SinaBrauch <sina.brauch@gmail.com>
2022-03-23 15:29:33 +0100

    pyramid shape created and merged with tree branch

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let button = document.getElementsByClassName('btn');
+let pyramid = document.getElementsByClassName('pyaramid');
+
+function changeShape() {
+
+}
+
+button.addEventListener('click', changeShape, false);
