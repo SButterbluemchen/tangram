ed9eac3f51d8379a39e39f71f9384fb50f953cbc
SinaBrauch <sina.brauch@gmail.com>
2022-03-23 20:15:33 +0100

    console.logs deleted

--- js/main.js
+++ js/main.js
@@ -2,13 +2,10 @@ let buttons = document.getElementsByClassName('btn');
 let cg = document.querySelector('#container-general');
 
 function changeShape(event) {
-  let newClassName = event.target.className
-    .split(' ')[1];
+  let newClassName = event.target.className.split(' ')[1];
   cg.className = newClassName;
 }
 
-console.log(buttons);
 for (let button of buttons) {
-  console.log(button);
   button.addEventListener('click', changeShape, false)
 }
