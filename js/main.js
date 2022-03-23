ec6d68973a4b1583e84b1b167dea8ee81e718f7b
SinaBrauch <sina.brauch@gmail.com>
2022-03-23 20:08:04 +0100

    js code for button change added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-let button = document.getElementsByClassName('btn');
-let pyramid = document.getElementsByClassName('pyaramid');
+let buttons = document.getElementsByClassName('btn');
+let cg = document.querySelector('#container-general');
 
-function changeShape() {
-
+function changeShape(event) {
+  let newClassName = event.target.className
+    .split(' ')[1];
+  cg.className = newClassName;
 }
 
-button.addEventListener('click', changeShape, false);
+console.log(buttons);
+for (let button of buttons) {
+  console.log(button);
+  button.addEventListener('click', changeShape, false)
+}
