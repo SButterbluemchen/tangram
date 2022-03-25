e2afcbf8263cfc34d616875b2a46e700a66cdc5d
Kadiatou <kadidans@gmail.com>
2022-03-25 15:48:08 +0100

    add modificaton

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 let buttons = document.getElementsByClassName('btn');
 let cg = document.querySelector('#container-general');
+let currentButton;
 
 function changeShape(event) {
   let newClassName = event.target.className.split(' ')[1];
   cg.className = newClassName;
+ 
+  currentButton?.classList.remove('active');
+  if (currentButton === event.target) {
+    currentButton = undefined;
+    cg.className = '';
+  } else {
+    event.target.classList.add('active');
+    currentButton = event.target;
+  }
 }
 
 for (let button of buttons) {
